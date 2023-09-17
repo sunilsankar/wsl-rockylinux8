1341527bdda7c59230a3995a1296739d29d4fba8
Sunil Sankar <sunil@sunil.cc>
2023-09-17 13:29:23 +0200

    pushing the syntax error

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,2 @@
 curl https://download.rockylinux.org/stg/rocky/8.8/isos/x86_64/Rocky-x86_64-boot.iso -o /tmp/boot.iso
-sudo livemedia-creator --make-tar --iso=Rocky-x86_64-boot.iso --ks=rockylinux.ks --image-name=rockylinux8-docker.tar.xz
+sudo livemedia-creator --make-tar --iso=/tmp/boot.iso --ks=rockylinux.ks --image-name=rockylinux8-docker.tar.xz
