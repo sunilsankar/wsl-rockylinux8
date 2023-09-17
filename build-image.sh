6e47942dd2afdcf36e8ad970e56a147b4a00b049
Sunil Sankar <sunil@sunil.cc>
2023-09-17 15:15:23 +0200

    Fixed the syntax

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,3 @@
 curl https://download.rockylinux.org/stg/rocky/8.8/isos/x86_64/Rocky-x86_64-boot.iso -o /tmp/boot.iso
-livemedia-creator --make-tar --iso=/tmp/boot.iso --ks=${{ github.workspace }}/rockylinux.ks --image-name=rockylinux8-docker.tar.xz
+pwd && ls -l
+livemedia-creator --make-tar --iso=/tmp/boot.iso --ks=rockylinux.ks --image-name=rockylinux8-docker.tar.xz
