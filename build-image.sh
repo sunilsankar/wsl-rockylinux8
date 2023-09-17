d20c6294b67bfcb72a3bf9e2f01ccc2599fcec3b
Sunil Sankar <sunil@sunil.cc>
2023-09-17 15:52:35 +0200

    Fixed the code

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
-curl https://download.rockylinux.org/stg/rocky/8.8/isos/x86_64/Rocky-x86_64-boot.iso -o /tmp/boot.iso
+curl https://download.rockylinux.org/stg/rocky/8.8/isos/x86_64/Rocky-x86_64-boot.iso -o boot.iso
 pwd && ls -l
-livemedia-creator --make-tar --iso=/tmp/boot.iso --ks=rockylinux.ks --image-name=rockylinux8-docker.tar.xz
+livemedia-creator --make-tar --iso=boot.iso --ks=rockylinux.ks --image-name=rockylinux8-docker.tar.xz
